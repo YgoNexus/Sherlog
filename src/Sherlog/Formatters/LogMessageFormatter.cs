9f096885febfb2ffbf0b853bcb69fc0488c5202d
ygonexus <ygonexus0@gmail.com>
2024-09-02 22:12:23 +0800

    integrated into sherlog.csproj

---
+++ src/Sherlog/Formatters/LogMessageFormatter.cs
@@ -0,0 +1,14 @@
+namespace Sherlog.Formatters
+{
+    public class LogMessageFormatter
+    {
+        readonly string _format;
+
+        public LogMessageFormatter(string format = "[{1}] {2}") => _format = format;
+        // do not care about logger.Name
+        //public LogMessageFormatter(string format = "[{1}] {0} {2}") => _format = format;
+
+        public string FormatMessage(Logger logger, LogLevel logLevel, string message) =>
+            string.Format(_format, logger.Name, logLevel, message);
+    }
+}
---
+++ src/Sherlog/Formatters/StackInfoFormatter.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Sherlog.Formatters
+{
+    public class StackInfoFormatter
+    {
+        private readonly string _format;
+        public StackInfoFormatter(string format = null)
+        {
+            if (format == null)
+                // do not care about {0}, stack info contains that message
+                format = $"{{0}}{Environment.NewLine}=> {{1}}";
+            _format = format;
+        }
+
+        public string FormatMessage(Logger logger, LogLevel logLevel, string message)
+        {
+            StackTrace stackTrace = new(true);
+            // filter LLog and Sherlog from stackTrace
+            string filteredStackTrace = string.Join(Environment.NewLine,
+                stackTrace.GetFrames()
+                .Where(frame =>
+                !frame.GetMethod().DeclaringType.FullName.ToUpper().Contains("LLOG", StringComparison.OrdinalIgnoreCase) &&
+                !frame.GetMethod().DeclaringType.FullName.ToUpper().Contains("SHERLOG", StringComparison.OrdinalIgnoreCase))
+                .Select(frame => frame.ToString()));
+
+            var result = $"{string.Format(_format, message, filteredStackTrace + Environment.NewLine)} ";
+            return result;
+        }
+    }
+}
